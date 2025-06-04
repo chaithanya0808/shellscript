b13182d5cda6c912d397220ef427c2c40d4d71bf
chaithanya kumar <chaithanya.reddy12@gmail.com>
2025-06-04 15:19:22 +0530

    variable test

---
+++ 03-variables.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+echo "Please enter your username ::"
+read -s USERNAME
+echo "your username is :: $USERNAME"
+echo "Please enter your credentials ::"
+read -s PASSWORD
+echo "your password is :: $PASSWORD"
